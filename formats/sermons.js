19c3eb79383eddeec500deba9e3b3401fe196ec5
Andrew Young <andrewdyoung@gmail.com>
2014-06-09 00:16:22 -0700

    Add sermons.js

---
+++ formats/sermons.js
@@ -0,0 +1,56 @@
+var $ = require('cheerio');
+var _ = require('underscore');
+
+// parses a typical four column table for assets
+// as found on http://christurc.org/sermons_genesis.html
+// date | passage | title | link
+exports.parse = function(html, sermonPage) {
+  var assets = [];
+  var $html = $.load(html);
+  var tds = $html('td.regtext');
+  var series;
+
+  _.each(tds, function(td){
+    var $tr = $(td).parent();
+    var tds = $('td', $tr);
+    var strong = $('strong, .bold', tds);
+
+    // check if this is a series title
+    if(strong.length){
+      series = strong.text().replace(',', '').replace(/\s+/, ' ');
+      console.log('SERIES FOUND: %s', series);
+      return;
+    }
+
+    var text = tds.eq(0).text().replace(/\s{2,}/g, ' ');
+
+    var matches = /^(.+?)( ?— ?| ?– ?)(.+?)( ?— ?| ?– ?)(.*?)$/.exec(text);
+
+    if(!matches) {
+      return;
+    }
+
+    var asset = {
+      date: matches[5],
+      passages: matches[1],
+      title: matches[3],
+      uri: $('a.link', tds).attr('href') || $('a', tds).attr('href'),
+      speaker: sermonPage.defaultSpeaker,
+      tags: sermonPage.tags.slice(0),
+      categories: [series]
+    };
+
+    // asset.passages.push(fixWhiteSpace(tds.eq(1).text()).trim());
+
+    asset.title = asset.title.replace(/“|”/g, '').trim();
+    asset.title = asset.title.replace(/\"/g, '');
+    assets.push(asset);
+  });
+
+  return assets;
+};
+
+
+function fixWhiteSpace(str) {
+  return str.replace(/\s+/g, ' ');
+}
--- sermonPages.js
+++ sermonPages.js
@@ -7,187 +7,195 @@ var parseThreePsalms = require('./formats/threeColumnPsalms').parse;
 var parseThreeOfficers = require('./formats/threeColumnOfficers').parse;
 var parseThreeCatechismHorton = require('./formats/threeColumnCatechismHorton').parse;
 var parseThreeGuest = require('./formats/threeColumnGuest').parse
+var parseSermons = require('./formats/sermons').parse
 var defaultSpeaker = 'Rev. Michael Brown';
 
 module.exports = [
+  // {
+  //   tags: ['Genesis', 'Morning', 'Sermon'],
+  //   page: 'sermons_genesis.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Exodus', 'Morning', 'Sermon'],
+  //   page: 'sermons_exodus.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Gospel of Luke', 'Morning', 'Sermon'],
+  //   page: 'sermons_luke.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Gospel of John', 'Evening', 'Sermon'],
+  //   page: 'sermons_john.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Drama of Redemption', 'Morning', 'Sermon'],
+  //   page: 'sermons_redemption.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Topical', 'Sermon'],
+  //   page: 'sermons_single.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFourTopical
+  // },
+
+  // {
+  //   tags: ['Sermons by Dr. Horton', 'Sermon'],
+  //   page: 'sermons_horton.html',
+  //   defaultSpeaker: 'Rev. Dr. Michael Horton',
+  //   parse: parseThree
+  // },
+
+  // {
+  //   tags: ['Seasonal', 'Sermon'],
+  //   page: 'sermons_seasonal.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFourSeasonal
+  // },
+
+  // {
+  //   tags: ['Psalms', 'Evening', 'Sermon'],
+  //   page: 'sermons_psalms.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreePsalms
+  // },
+
+  // {
+  //   tags: ['Acts of the Apostles', 'Evening', 'Sermon'],
+  //   page: 'sermons_acts.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThree
+  // },
+
+  // {
+  //   tags: ['Doctrines of Grace', 'Evening', 'Sermon'],
+  //   page: 'sermons_dort.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreeGrace
+  // },
+
+  // {
+  //   tags: ['Church Officers', 'Evening', 'Sermon'],
+  //   page: 'sermons_officers.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreeOfficers
+  // },
+
+  // {
+  //   tags: ['Catechism', 'Sunday School'],
+  //   page: 'catechism_horton.html',
+  //   defaultSpeaker: 'Rev. Dr. Michael Horton',
+  //   parse: parseThreeCatechismHorton
+  // },
+
+  // // this one needs some manual work
+  // {
+  //   tags: ['Catechism', 'Guest Speaker'],
+  //   page: 'sermons_guest.html',
+  //   defaultSpeaker: '',
+  //   parse: parseThreeGuest
+  // },
+
+  // // this one needs some manual work
+  // {
+  //   tags: ['Catechism', 'Interns'],
+  //   page: 'sermons_interns.html',
+  //   defaultSpeaker: '',
+  //   parse: parseThreeGuest
+  // },
+
+  // {
+  //   tags: ['Colossians', 'Morning', 'Sermon'],
+  //   page: 'sermons_colossians.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Galatians', 'Morning', 'Sermon'],
+  //   page: 'sermons_galatians.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Hebrews', 'Morning', 'Sermon'],
+  //   page: 'sermons_hebrews.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  //  // needs a bit of manual work
+  // {
+  //   tags: ['Heidelberg Catechism', 'Evening', 'Sermon'],
+  //   page: 'sermons_heidelberg.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  //  // needs a bit of manual work
+  // {
+  //   tags: ['Leadership', 'Evening', 'Sermon'],
+  //   page: 'sermons_leadership.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreeGrace
+  // },
+
+  // {
+  //   tags: ['Philemon', 'Evening', 'Sermon'],
+  //   page: 'sermons_philemon.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreeGrace
+  // },
+
+  // {
+  //   tags: ['Philippians', 'Evening', 'Sermon'],
+  //   page: 'sermons_philippians.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreeGrace
+  // },
+
+  // {
+  //   tags: ['Prayer', 'Sermon'],
+  //   page: 'sermons_prayer.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['2 Timothy', 'Evening', 'Sermon'],
+  //   page: 'sermons_second_timothy.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseFour
+  // },
+
+  // {
+  //   tags: ['Zechariah', 'Evening', 'Sermon'],
+  //   page: 'sermons_zechariah.html',
+  //   defaultSpeaker: defaultSpeaker,
+  //   parse: parseThreeGrace
+  // },
+
   {
-    tags: ['Genesis', 'Morning', 'Sermon'],
-    page: 'sermons_genesis.html',
+    tags: ['Sermon'],
+    page: 'sermons.html',
     defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Exodus', 'Morning', 'Sermon'],
-    page: 'sermons_exodus.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Gospel of Luke', 'Morning', 'Sermon'],
-    page: 'sermons_luke.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Gospel of John', 'Evening', 'Sermon'],
-    page: 'sermons_john.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Drama of Redemption', 'Morning', 'Sermon'],
-    page: 'sermons_redemption.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Topical', 'Sermon'],
-    page: 'sermons_single.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFourTopical
-  },
-
-  {
-    tags: ['Sermons by Dr. Horton', 'Sermon'],
-    page: 'sermons_horton.html',
-    defaultSpeaker: 'Rev. Dr. Michael Horton',
-    parse: parseThree
-  },
-
-  {
-    tags: ['Seasonal', 'Sermon'],
-    page: 'sermons_seasonal.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFourSeasonal
-  },
-
-  {
-    tags: ['Psalms', 'Evening', 'Sermon'],
-    page: 'sermons_psalms.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreePsalms
-  },
-
-  {
-    tags: ['Acts of the Apostles', 'Evening', 'Sermon'],
-    page: 'sermons_acts.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThree
-  },
-
-  {
-    tags: ['Doctrines of Grace', 'Evening', 'Sermon'],
-    page: 'sermons_dort.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreeGrace
-  },
-
-  {
-    tags: ['Church Officers', 'Evening', 'Sermon'],
-    page: 'sermons_officers.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreeOfficers
-  },
-
-  {
-    tags: ['Catechism', 'Sunday School'],
-    page: 'catechism_horton.html',
-    defaultSpeaker: 'Rev. Dr. Michael Horton',
-    parse: parseThreeCatechismHorton
-  },
-
-  // this one needs some manual work
-  {
-    tags: ['Catechism', 'Guest Speaker'],
-    page: 'sermons_guest.html',
-    defaultSpeaker: '',
-    parse: parseThreeGuest
-  },
-
-  // this one needs some manual work
-  {
-    tags: ['Catechism', 'Interns'],
-    page: 'sermons_interns.html',
-    defaultSpeaker: '',
-    parse: parseThreeGuest
-  },
-
-  {
-    tags: ['Colossians', 'Morning', 'Sermon'],
-    page: 'sermons_colossians.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Galatians', 'Morning', 'Sermon'],
-    page: 'sermons_galatians.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Hebrews', 'Morning', 'Sermon'],
-    page: 'sermons_hebrews.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-   // needs a bit of manual work
-  {
-    tags: ['Heidelberg Catechism', 'Evening', 'Sermon'],
-    page: 'sermons_heidelberg.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-   // needs a bit of manual work
-  {
-    tags: ['Leadership', 'Evening', 'Sermon'],
-    page: 'sermons_leadership.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreeGrace
-  },
-
-  {
-    tags: ['Philemon', 'Evening', 'Sermon'],
-    page: 'sermons_philemon.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreeGrace
-  },
-
-  {
-    tags: ['Philippians', 'Evening', 'Sermon'],
-    page: 'sermons_philippians.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreeGrace
-  },
-
-  {
-    tags: ['Prayer', 'Sermon'],
-    page: 'sermons_prayer.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['2 Timothy', 'Evening', 'Sermon'],
-    page: 'sermons_second_timothy.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseFour
-  },
-
-  {
-    tags: ['Zechariah', 'Evening', 'Sermon'],
-    page: 'sermons_zechariah.html',
-    defaultSpeaker: defaultSpeaker,
-    parse: parseThreeGrace
-  },
+    parse: parseSermons
+  }
 
 ];
 
